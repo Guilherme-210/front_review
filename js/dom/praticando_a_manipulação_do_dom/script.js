eacc8663a78d852e0bf86e7c164f06ce616411f7
Guilherme-210 <guireis1164@gmail.com>
2024-10-01 17:12:35 -0300

    feat: added the lesson on DOM manipulation

---
+++ js/dom/praticando_a_manipulação_do_dom/script.js
@@ -0,0 +1,66 @@
+// Adicionando um contato a lista
+
+function addContact() {
+  const contactSection = document.getElementById("contacts-list")
+
+  const h3 = document.createElement("h3")
+  h3.innerText = "Contato"
+
+  const ul = document.createElement("ul")
+
+  const nameLi = document.createElement("li")
+  nameLi.innerText = "Nome: "
+  const nameInput = document.createElement("input")
+  nameInput.type = "text"
+  nameInput.name = "fullname"
+  nameLi.appendChild(nameInput)
+  ul.appendChild(nameLi)
+  ul.appendChild(document.createElement("br"))
+
+  const phoneLi = document.createElement("li")
+  phoneLi.innerText = "Telefone: "
+  const phoneInput = document.createElement("input")
+  phoneInput.type = "text"
+  phoneInput.name = "phone"
+  phoneLi.appendChild(phoneInput)
+  ul.appendChild(phoneLi)
+  ul.appendChild(document.createElement("br"))
+
+  const addressLi = document.createElement("li")
+  addressLi.innerHTML = '<label for="address">Endereço: </label>'
+  const addressInput = document.createElement("input")
+  addressInput.type = "text"
+  addressInput.name = "address"
+  addressInput.id = "address"
+  addressLi.appendChild(addressInput)
+  ul.appendChild(addressLi)
+  ul.appendChild(document.createElement("br"))
+
+  contactSection.append(h3, ul)
+}
+
+// Removendo um contato da lista
+
+// Codigo da aula, nâo remove completamente os contatos
+// function removeContact() {
+//   const contactSection = document.getElementById("contacts-list")
+
+//   const titles = document.getElementsByTagName("h3")
+//   const contacts = document.getElementsByTagName("ul")
+
+//   contactSection.removeChild(titles[titles.length - 1])
+//   contactSection.removeChild(contacts[contacts.length - 1])
+// }
+
+// Apos pesquisas, consegui resolver o problema de não estar removendo os imputs e nem seus respectivos textos de identificação.
+function removeContact() {
+  const contactSection = document.getElementById("contacts-list")
+
+  const lastH3 = contactSection.querySelector("h3:last-of-type")
+  const lastUl = contactSection.querySelector("ul:last-of-type")
+
+  if (lastH3 && lastUl) {
+    contactSection.removeChild(lastH3)
+    contactSection.removeChild(lastUl)
+  }
+}
