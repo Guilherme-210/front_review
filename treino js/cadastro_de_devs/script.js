d750effbf60a986e2883042aa74d26eb02b9d8f6
Guilherme Reis <guireis1164@gmail.com>
2025-02-08 11:42:28 -0300

    Feat: All required functionality has been added to JavaScript, Refactor: Spelling corrections have been made and explanatory comments have been added to each function

--- treino js/cadastro_de_devs/script.js
+++ treino js/cadastro_de_devs/script.js
@@ -1,12 +1,14 @@
+// função para criação de label
 function createLabel(text, htmlFor) {
-  const label = document.createElement('label')
+  const label = document.createElement("label")
   label.htmlFor = htmlFor
-  label.innerText = text 
+  label.innerText = text
   return label
 }
 
-function createImput(id, value, name, type = 'text', placeholder = '') {
-  const input = document.createElement('input')
+// função para criação de label
+function createInput(id, value, name, type = "text", placeholder = "") {
+  const input = document.createElement("input")
   input.id = id
   input.value = value
   input.name = name
@@ -15,26 +17,98 @@ function createImput(id, value, name, type = 'text', placeholder = '') {
   return input
 }
 
+// Criação das variaveis globais
 const add__technology = document.getElementById("add__technology")
 const form = document.getElementById("dev__form")
 const developers = []
 let inputRows = 0
 
-add__technology.addEventListener('click', function (ev) {
+// Evento de criação das labels e inputs
+add__technology.addEventListener("click", function (ev) {
   const stackInput = document.getElementById("stack__inputs")
 
-  const newRow = document.createElement('li')
+  // Criação da lista
+  const newRow = document.createElement("li")
   const rowIndex = inputRows
   inputRows++
-  newRow.id = 'inputRow-' + rowIndex
-  newRow.className = 'inputRow'
+  newRow.id = "inputRows-" + rowIndex
+  newRow.className = "inputRows"
 
-  const techNameLabel = createLabel('Nome: ', 'techName-' + rowIndex)
-  const tachNameInput = createImput('techName-' + rowIndex, null, 'techName')
+  // Criação da input do nome das materias
+  const techNameLabel = createLabel("Nome: ", "techName-" + rowIndex)
+  const tachNameInput = createInput("techName-" + rowIndex, null, "techName")
 
+  // Criação dos opições de tempo de esperiencia
+  const expLabel = createLabel("Nome: ", "techName-" + rowIndex)
+  // Radio 1
+  const id1 = "expRadio-" + rowIndex + ".1"
+  const expRadio1 = createInput(id1, "0-2 anos", "techExp-" + rowIndex, "radio")
+  const expLabel1 = createLabel("0-2 anos", id1)
+  // Radio 2
+  const id2 = "expRadio-" + rowIndex + ".2"
+  const expRadio2 = createInput(id2, "3-4 anos", "techExp-" + rowIndex, "radio")
+  const expLabel2 = createLabel("3-4 anos", id2)
+  // Radio 3
+  const id3 = "expRadio-" + rowIndex + ".3"
+  const expRadio3 = createInput(id3, "5+ anos", "techExp-" + rowIndex, "radio")
+  const expLabel3 = createLabel("5+ anos", id3)
+
+  // Criando botão de remoção
+  const removRowBtn = document.createElement("button")
+  removRowBtn.type = "button"
+  removRowBtn.innerText = "Remove"
+  removRowBtn.addEventListener("click", function () {
+    stackInput.removeChild(newRow)
+  })
+
+  // Adicionando elementos na newRow
   newRow.append(
-    techNameLabel, tachNameInput
+    techNameLabel,
+    tachNameInput,
+    expLabel,
+    expRadio1,
+    expLabel1,
+    expRadio2,
+    expLabel2,
+    expRadio3,
+    expLabel3,
+    removRowBtn
   )
 
+  // Colocando os elementos da newRow a página
   stackInput.appendChild(newRow)
+})
+
+// Adicionando o evento ao formulario
+form.addEventListener("submit", function (ev) {
+  ev.preventDefault()
+  // Capitação dos valores do formulario
+  const fullnameInput = document.getElementById("fullname")
+  const inputRows = document.querySelectorAll(".inputRows")
+
+  // Criação do obijeto com os valores de cada uma das linhas do formulario
+  let technologies = []
+  inputRows.forEach(function (row) {
+    const techName = document.querySelector(
+      "#" + row.id + ' input[name="techName"]'
+    ).value
+    const tecExp = document.querySelector(
+      "#" + row.id + ' input[type="radio"]:checked'
+    ).value
+
+    technologies.push({ name: techName, exp: tecExp })
+  })
+
+  // Cadastrando um novo desenvolvedor
+  const newDev = {fullname: fullnameInput.value, technologies: technologies}
+  developers.push(newDev)
+  alert('Dev cadastrado com sucesso!')
+
+  // Limpeza do formulario
+  fullnameInput.value = ''
+  inputRows.forEach(function (row) {
+    row.remove()
+  })
+
+  console.log(developers)
 })
