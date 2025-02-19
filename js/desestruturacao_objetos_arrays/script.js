08ddbfdd77e25456546d6df01c8caf4da41ee520
Guilherme Reis <guireis1164@gmail.com>
2025-02-19 11:20:05 -0300

    refactor(structure): Refactors page structure and adds theme switching functionality

--- js/desestruturacao_objetos_arrays/script.js
+++ js/desestruturacao_objetos_arrays/script.js
@@ -1,32 +1,4 @@
-// const person = {
-//   name: "Luke",
-//   job: "Farmer",
-//   parents: ["Anakin", "Padme"],
-// }
-
-// const name = person.name
-
-// const { job, parents } = person
-
-// console.log(name, job, parents)
-
-// const [father, mother] = parents
-
-// console.log(father, mother)
-
-// function createUser({ name, job, parents }) {
-//   const id = Math.floor(Math.random() * 9999)
-//   return {
-//     id,
-//     name,
-//     job,
-//     parents,
-//   }
-// }
-
-// const luke = createUser(person)
-
-// console.log(luke)
+// Exemplo usado na aula
 
 const person = {
   name: "Luke",
@@ -35,13 +7,10 @@ const person = {
 }
 
 const name = person.name
-
 const { job, parents } = person
-
 console.log(name, job, parents)
 
 const [father, mother] = parents
-
 console.log(father, mother)
 
 function createUser({ name, job, parents }) {
@@ -55,5 +24,18 @@ function createUser({ name, job, parents }) {
 }
 
 const luke = createUser(person)
-
 console.log(luke)
+
+
+// Exemplo de treino: Extraindo dados de um objeto de carro
+const car = {
+  brand: "Tesla",
+  model: "Model S",
+  specs: {
+    horsepower: 1020,
+    range: "396 miles"
+  }
+}
+
+const { brand, model, specs: { horsepower, range } } = car
+console.log(brand, model, horsepower, range)
