9da3fa0b88acba1fd0c170dc3853226352d3b1be
Guilherme Reis <guireis1164@gmail.com>
2025-02-21 10:45:34 -0300

    feat(clearButton): display Clear button only when there is content in the textArea

--- treino_js/objetos/script.js
+++ treino_js/objetos/script.js
@@ -1,6 +1,8 @@
 const form = document.getElementById("form")
 const textArea = document.getElementById("textAlunos")
-let alunos = [] // Array para armazenar os alunos
+clearButton = document.getElementById("clearButton");
+let alunos = [] // Array para armazenar os alunosconst 
+
 
 form.addEventListener("submit", function (ev) {
   ev.preventDefault()
@@ -23,17 +25,7 @@ form.addEventListener("submit", function (ev) {
   document.getElementById("inputAge").value = ""
   document.getElementById("inputMatter").value = ""
   document.getElementById("inputTemp").value = ""
-})
-
-
-const textAlunos = parseFloat(document.getElementById("textAlunos").value)
-
-document.getElementById("clearButton").addEventListener("click", function () {
-  textArea.value = ""
-
-  setTimeout(function () {
-    val_1.focus()
-  }, 1000) // 1000 milissegundos = 1 segundos
+  document.getElementById("inputName").focus()
 })
 
 
@@ -42,15 +34,32 @@ document.getElementById("pullButton").addEventListener("click", function () {
   alunos.forEach((aluno) => {
     texto += `Nome: ${aluno.nome},\nIdade: ${aluno.idade}, \nMatéria: ${aluno.materia},\nTempo de estudos: ${aluno.temp}\n\n`
   })
-
+  
   document.getElementById("carregamento").style.display = "block"
 
   setTimeout(function () {
     // Exibe o resultado na text area
     textArea.value += texto
 
+    // Mostra o botão "Apagar"
+    clearButton.style.display = "inline-block"
 
     // Oculta o elemento de carregamento após a conclusão
     document.getElementById("carregamento").style.display = "none"
   }, 2000)
 })
+
+  
+  const textAlunos = parseFloat(document.getElementById("textAlunos").value)
+  
+  document.getElementById("clearButton").addEventListener("click", function () {
+    // Limpa a textArea
+    textArea.value = "" 
+
+    // Esconde o botão "Apagar"
+    clearButton.style.display = "none"
+
+    setTimeout(function () {
+      val_1.focus()
+    }, 1000) // 1000 milissegundos = 1 segundos
+  })
