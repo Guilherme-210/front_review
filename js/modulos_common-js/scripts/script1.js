c574f50318d1a684124ba97df3ca0a1b428fe485
Guilherme Reis <guireis1164@gmail.com>
2025-03-06 20:06:14 -0300

    Feat(Page): Implements complete Common.JS Modules page structure

---
+++ js/modulos_common-js/scripts/script1.js
@@ -0,0 +1,12 @@
+function render1() {
+  console.log('Renderizando a interface da aplicação...')
+}
+
+function store1() {
+  console.log("Salvando as informações no banco de dados...")
+}
+
+console.log("Aplicação 1 iniciada.")
+render1()
+store1()
+console.log("Aplicação finalizada.")
---
+++ js/modulos_common-js/scripts/script2.js
@@ -0,0 +1,9 @@
+//requerimento de inportação
+
+const render = require("./render.js")
+const store = require("./store.js")
+
+console.log("Aplicação 2 iniciada.")
+render()
+store()
+console.log("Aplicação finalizada.")
